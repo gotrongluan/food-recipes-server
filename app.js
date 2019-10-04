964b18011d69fa4a534bc32440ec7f555e522817
bkuhardware <luannguyentrong98@gmail.com>
2019-10-04 18:56:50 +0700

    Config environment variables

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const debug = require('debug')('app:app');
-// const config = require('config');
+const debug = require('debug')('food-recipes-server:app');
+const config = require('config');
+const connectToDatabase = require('./utils/connectToDatabase');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,6 +13,8 @@ const foodsRouter = require('./routes/foods');
 const foodTypesRouter = require('./routes/foodTypes');
 
 const app = express();
+
+connectToDatabase(config.get('mongoUrl'), (db) => debug('Connected to MongoDB Server...'), (err) => debug('Cannot connect to MongoDB Server!\n', err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
