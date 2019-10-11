511ebc182f7c076ace5ded48dafc63c1ce7f38f6
bkuhardware <luan.nt@teko.vn>
2019-10-11 19:00:05 +0700

    Passport, passport local

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
+const passport = require('passport');
 const debug = require('debug')('food-recipes-server:users');
 const wrapResponse = require('../utils/wrapResponse');
 const { User, validate } = require('../models/users');
@@ -12,68 +13,24 @@ router.post('/signup', async (req, res, next) => {
 	debug(error);
 	if (error)
 		return next(createError(400));
-	try {
-		const user = await User.findOne({ username: req.body.username });
-		if (user !== null) {
-			debug(`Username ${req.body.username} is already exist!`);
-			res.successJson(wrapResponse(null));
+	//Best practice when using register method of User is use callback
+	User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+		if (err) {
+			return next(err);
 		}
-		else {
-			let newUser = new User({
-				username: req.body.username,
-				password: req.body.password,
-			});
-			newUser =  await newUser.save();
-			res.successJson(wrapResponse(newUser));
-		}
-	}
-	catch(err) {
-		next(err);
-	}
+		passport.authenticate('local')(req, res, () => {
+			res.successJson(wrapResponse('Successful!'));
+		})
+	});
 });
 
-router.post('/login', async (req, res, next) => {
-	if (!req.session.user) {
-		const authHeader = req.headers.authorization;
-		if (!authHeader) {
-			const error = new Error('You are not authenticated!');
-			error.status = 401;
-			res.setHeader('WWW-Authenticate', 'Basic');
-			next(error);
-		}
-		//get authorized data
-		//authHeader = 'Basic base64...';
-		const authData = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-		const username = authData[0];
-		const password = authData[1];
-
-		try {
-			const user = await User.findOne({ username: username });
-			if (user !== null) {
-				if (user.password === password) {
-					//Save cookie, session
-					debug('Login successfully!');
-					req.session.user = 'authenticated';
-					res.successJson(wrapResponse(user));
-				}
-			}
-			const error = new Error('Your username or password is incorrect!');
-			error.status = 401;
-			res.setHeader('WWW-Authenticate', 'Basic');
-			next(error);
-		}
-		catch(err) {
-			next(err);
-		}
-	}
-	else {
-		debug("Session is already exist!");
-		res.successJson(wrapResponse(null));
-	}
+router.post('/login', passport.authenticate('local'), (req, res) => {
+	//if this callback is called, authentication was successful!!
+	res.successJson(wrapResponse(req.user));
 });
 
 router.get('/logout', async (req, res, next) => {
-	if (req.session.user) {
+	if (req.user) {
 		req.session.destroy();
 		res.clearCookie('food-recipes-server-session-id');
 	}
